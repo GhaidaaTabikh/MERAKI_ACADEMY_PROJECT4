3621ac388e5a5b85937a2c2d477e1e727683cd4e
Ghaidaa_tabikh <tabikh.ghaidaa@gmail.com>
2021-06-09 19:30:31 +0300

    use token

--- frontend/src/components/auth/login/index.js
+++ frontend/src/components/auth/login/index.js
@@ -5,7 +5,6 @@ import "./login.css";
 
 import { useHistory } from "react-router-dom";
 
-
 export default function Login({ setToken, path }) {
   const history = useHistory();
   const [email, setEmail] = useState("");
@@ -21,11 +20,9 @@ export default function Login({ setToken, path }) {
       .then((res) => {
         console.log(res);
         if (res.data.token) {
-<<<<<<< HEAD
-          //setToken(res.data.token);
-=======
+          console.log(res.data.token);
+        setToken(res.data.token);
 
->>>>>>> 2130974da03cb69b0c70f77c940c4732ffbe20bd
           setLoginMessage("login successful");
           history.push(path);
         } else {
@@ -35,74 +32,45 @@ export default function Login({ setToken, path }) {
       .catch((err) => {
         console.log(err);
         setLoginMessage("please try again");
-
       });
   };
 
   return (
-<<<<<<< HEAD
-    <div>
-      <p>Login</p>
-      <p>Login to your account</p>
-      <fieldset>
-        <legend>E-mail</legend>
-      <input
-        type="text"
-        placeholder="email here"
-        onChange={(e) => {
-          setEmail(e.target.value);
-        }}
-      />
-     </fieldset>
+    <div className="outLogin">
+      <div className="login">
+        <h2 style={{ textAlign: "center" }}>Login</h2>
+        <h4 style={{ textAlign: "center" }}>Login to your account</h4>
+        <fieldset>
+          <legend>E-mail</legend>
+          <input
+            className="input"
+            type="text"
+            placeholder="email here"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+        </fieldset>
 
-     <fieldset>
-        <legend>Password</legend>
-      <input
-        type="password"
-        placeholder="password here"
-        onChange={(e) => {
-          setPassword(e.target.value);
-        }}
-      />
-       </fieldset>
-=======
+        <fieldset>
+          <legend>Password</legend>
+          <input
+            className="input"
+            type="password"
+            placeholder="password here"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+        </fieldset>
 
-    <div className="outLogin" >
-    <div className="login" >
-      <h2 style={{ textAlign: "center" }} >Login</h2>
-      <h4 style={{ textAlign: "center" }} >Login to your account</h4>
-      <fieldset>
-        <legend>E-mail</legend>
-        <input
-          className="input"
-          type="text"
-          placeholder="email here"
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
-        />
-      </fieldset>
-
-      <fieldset>
-        <legend>Password</legend>
-        <input
-        className="input"
-          type="password"
-          placeholder="password here"
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-        />
-      </fieldset>
-
-  
->>>>>>> 2130974da03cb69b0c70f77c940c4732ffbe20bd
-      <button className="loginButton" onClick={loginFun}>
-        LOGIN
-      </button>
-      <h4 className="message"  style={{ textAlign: "center" }} >{loginMessage}</h4>
-    </div>
+        <button className="loginButton" onClick={loginFun}>
+          LOGIN
+        </button>
+        <h4 className="message" style={{ textAlign: "center" }}>
+          {loginMessage}
+        </h4>
+      </div>
     </div>
   );
 }
-
